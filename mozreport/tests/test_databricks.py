55a9a37859f09894cfdfbd2fb55c96e0d3c3197f
Tim Smith <tdsmith@mozilla.com>
2018-09-25 14:01:19 -0700

    Add stubby smoke tests

--- mozreport/tests/test_databricks.py
+++ mozreport/tests/test_databricks.py
@@ -1,6 +1,8 @@
 from io import BytesIO
+from unittest.mock import Mock, create_autospec
 
 import pytest
+import requests
 
 from mozreport import databricks
 from mozreport.cli import CliConfig
@@ -30,3 +32,30 @@ class TestDatabricksIntegration:
         assert client.file_exists(path)
         client.delete_file(path)
         assert not client.file_exists(path)
+
+
+class TestDatabricks:
+    @pytest.fixture
+    def mocked_client(self):
+        config = databricks.DatabricksConfig(token="token", host="host")
+        session = create_autospec(requests.Session())
+        for method in ("get", "post"):
+            getattr(session, method).return_value = Mock(status_code=200)
+        client = databricks.Client(config=config, session=session)
+        return (client, session)
+
+    def test_upload_file(self, mocked_client):
+        client, session = mocked_client
+        contents = BytesIO(b"Hello, world!")
+        client.upload_file(contents, "/my_file.txt")
+        session.post.assert_called_once()
+
+    def test_file_exists(self, mocked_client):
+        client, session = mocked_client
+        client.file_exists("/foo")
+        session.get.assert_called_once()
+
+    def test_delete_file(self, mocked_client):
+        client, session = mocked_client
+        client.delete_file("/foo")
+        session.post.assert_called_once()
