555cfbf4cfa553e57aaaee6840beea9543151411
Tim Smith <tdsmith@mozilla.com>
2018-11-23 09:39:14 -0800

    Allow only observing after enrollment ends

--- mozreport/etl_script.py
+++ mozreport/etl_script.py
@@ -15,7 +15,7 @@ def name_to_stub(name):
     return re.sub(r"[^A-Za-z0-9]+", "_", name).strip("_").lower()
 
 
-def run_etl(slug, branches, output_path):
+def run_etl(slug, branches, enrollment_end, output_path):
     from mozanalysis import metrics
     from mozanalysis.experiments import ExperimentAnalysis
 
@@ -28,7 +28,9 @@ def run_etl(slug, branches, output_path):
 
     experiments = spark.table("experiments")  # noqa
     my_experiment = experiments.filter(experiments.experiment_id == slug)
-    analysis = ExperimentAnalysis(my_experiment).metrics(*blessed_metrics).run()
+    if enrollment_end:
+        my_experiment = my_experiment.filter(my_experiment.submission_date_s3 > enrollment_end)
+    summary = ExperimentAnalysis(my_experiment).metrics(*blessed_metrics).run()
 
     if not os.path.exists(os.path.dirname(output_path)):
         os.makedirs(os.path.dirname(output_path))
@@ -39,8 +41,9 @@ def run_etl(slug, branches, output_path):
 @click.option("--branch", "branches", multiple=True, required=True, type=str)
 @click.option("--slug", required=True, type=str)
 @click.option("--uuid", required=True, type=str)
+@click.option("--enrollment-end", type=str)
 @click.option("--test", is_flag=True)
-def cli(slug, uuid, branches, test):
+def cli(slug, uuid, branches, enrollment_end, test):
     safe_slug = name_to_stub(slug)
     output_path = os.path.join(
         "/",
@@ -51,10 +54,11 @@ def cli(slug, uuid, branches, test):
     )
     if test:
         print("Slug:", slug)
+        print("Last day of enrollment period:", enrollment_end)
         print("Branches:", branches)
         print("Output path:", output_path)
         sys.exit(0)
-    run_etl(slug, branches, output_path)
+    run_etl(slug, branches, enrollment_end, output_path)
 
 
 if __name__ == "__main__":
