00f8f8a72f03628c6a4c02a28dca18adf05d3c5d
Tim Smith <tdsmith@mozilla.com>
2018-12-03 13:55:46 -0600

    Revert enrollment-period misfeature
    This isn't how these should work; oops!

--- mozreport/etl_template/etl_script.py
+++ mozreport/etl_template/etl_script.py
@@ -18,7 +18,7 @@ def name_to_stub(name):
     return re.sub(r"[^A-Za-z0-9]+", "_", name).strip("_").lower()
 
 
-def run_etl(slug, branches, enrollment_end, output_path):
+def run_etl(slug, branches, output_path):
     from mozanalysis import metrics
     from mozanalysis.experiments import ExperimentAnalysis
     from pyspark.sql import functions as f
@@ -32,8 +32,6 @@ def run_etl(slug, branches, enrollment_end, output_path):
 
     experiments = spark.table("experiments")  # noqa
     my_experiment = experiments.filter(experiments.experiment_id == slug)
-    if enrollment_end:
-        my_experiment = my_experiment.filter(my_experiment.submission_date_s3 > enrollment_end)
     summary = ExperimentAnalysis(my_experiment).metrics(*blessed_metrics).run()
 
     facets = [
@@ -81,9 +79,8 @@ def run_etl(slug, branches, enrollment_end, output_path):
 @click.option("--branch", "branches", multiple=True, required=True, type=str)
 @click.option("--slug", required=True, type=str)
 @click.option("--uuid", required=True, type=str)
-@click.option("--enrollment-end", type=str)
 @click.option("--test", is_flag=True)
-def cli(slug, uuid, branches, enrollment_end, test):
+def cli(slug, uuid, branches, test):
     safe_slug = name_to_stub(slug)
     output_path = os.path.join(
         "/",
@@ -94,11 +91,10 @@ def cli(slug, uuid, branches, enrollment_end, test):
     )
     if test:
         print("Slug:", slug)
-        print("Last day of enrollment period:", enrollment_end)
         print("Branches:", branches)
         print("Output path:", output_path)
         sys.exit(0)
-    run_etl(slug, branches, enrollment_end, output_path)
+    run_etl(slug, branches, output_path)
 
 
 if __name__ == "__main__":
