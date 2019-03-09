296556b3d1acb3c9891dc5367c1a99c36dc9e50f
Tim Smith <tdsmith@mozilla.com>
2019-03-08 17:01:28 -0800

    Bump version to 19.3.1.dev0

--- mozreport/_version.py
+++ mozreport/_version.py
@@ -7,5 +7,5 @@ Provides mozreport version information.
 
 from incremental import Version
 
-__version__ = Version('mozreport', 18, 12, 1, dev=0)
+__version__ = Version('mozreport', 19, 3, 1, dev=0)
 __all__ = ["__version__"]
