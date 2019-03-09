8f0b0d43fd19b2882d0c363bc169c708ebe311c9
Tim D. Smith <github@tim-smith.us>
2019-03-08 16:23:46 -0800

    rmarkdown: Ensure RSQLite is installed (#46)
    Install RSQLite if it isn't present. Discovered while demoing mozreport
    to Emily.

--- mozreport/templates/rmarkdown/build.py
+++ mozreport/templates/rmarkdown/build.py
@@ -2,13 +2,16 @@
 import subprocess
 
 
+def R(script):
+    return subprocess.check_call(["R", "--slave", "-e", script])
+
+
 def build():
-    subprocess.check_call([
-        "R",
-        "--slave",
-        "-e",
-        "rmarkdown::render('report.Rmd')"
-    ])
+    R(
+        'if(!("RSQLite" %in% .packages(all=TRUE))) '
+        'install.packages("RSQLite", repo="https://cloud.r-project.org")'
+    )
+    R("rmarkdown::render('report.Rmd')")
 
 
 if __name__ == "__main__":
